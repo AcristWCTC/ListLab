bb666fb7cacff056751c7d9ea44c4a45e654f2b0
Adam <Adam@10.30.20.19>
2015-11-02 17:14:09 -0600

    Formatting Fixes

--- src/lists/Challenge2.java
+++ src/lists/Challenge2.java
@@ -17,13 +17,11 @@ public class Challenge2 {
         Employee e1 = new Employee("Nana", "Bob", "304-11-9246");
         Employee e2 = new Employee("Falsa", "Tim", "123-44-9215");
         Employee e3 = new Employee("Beleran", "Joe", "639-25-2256");
-        Employee e4 = new Employee("Bolas", "Susie", "669-25-7813");
         
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(e1);
         employeeList.add(e2);
         employeeList.add(e3);
-        employeeList.add(e4);
         
         
         for (Employee employ: employeeList){
